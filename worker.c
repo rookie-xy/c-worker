8c738bbdd47ea6041cf47d6859bcf2028b6fefa6
mengshi <meng1985626@163.com>
2016-12-22 15:37:47 +0800

    update code struct

---
+++ worker.c
@@ -0,0 +1,19 @@
+
+
+#include "types.h"
+
+
+int main(int argc const *char *argv const *char *envp)
+{
+    option_t *option;
+    worker_t *worker;
+
+    option = core_module_init(worker);
+
+    if () {
+    }
+
+    system_module_init();
+
+    user_module_init();
+}
